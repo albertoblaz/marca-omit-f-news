487de6d7d0235e0228e110ce9580843ca1b8383c
Alberto Blázquez <albertoblaz@users.noreply.github.com>
2024-07-03 10:52:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+const asURL = "https://as.com/noticias/futbol/femenino/"
+const marcaURL = "https://www.marca.com/futbol/futbol-femenino/"
+
 window.addEventListener("load", (event) => {
   Array
     .from(document.querySelectorAll("a"))
-    .filter(n =>
-      n.dataset["mrfLink"].startsWith("https://as.com/noticias/futbol/femenino/") ||
-      n.dataset["mrfLink"].startsWith("https://www.marca.com/futbol/futbol-femenino/")
-    )
+    .filter(n => {
+      const link = n.dataset["mrfLink"];
+      return link && (link.startsWith(asURL) || link.startsWith(marcaURL))
+    })
     .map(n => n.closest("article"))
     .forEach(n => n.remove())
 });
